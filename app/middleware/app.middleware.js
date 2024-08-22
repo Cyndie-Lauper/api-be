d1555ab9d4d9a9ac60a164e085a64038d0137f3e
Minh <elevenine00@gmail.com>
2024-08-23 00:40:55 +0700

    change some text
    change some text
    
    change some text

--- app/middleware/app.middleware.js
+++ app/middleware/app.middleware.js
@@ -5,7 +5,7 @@ import productRoutes from "../routes/product.routes.js";
 function registerRoutes(app) {
     app.use("/api", productRoutes);
     app.use("/api", categoryRoutes);
-    // app.use("/api", accountRoutes);
+    app.use("/api", accountRoutes);
 }
 
 export default registerRoutes;
