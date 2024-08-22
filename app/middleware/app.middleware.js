4e0c7ab31d425738c020af53815a0a86f9c256dd
Minh <elevenine00@gmail.com>
2024-08-22 11:49:01 +0700

    fix: bug not receiving data in api

--- app/middleware/app.middleware.js
+++ app/middleware/app.middleware.js
@@ -1,13 +1,10 @@
-import express from "express";
 // import accountRoutes from "../routes/account.routes.js";
-// import categoryRoutes from "../routes/category.routes.js";
+import categoryRoutes from "../routes/category.routes.js";
 import productRoutes from "../routes/product.routes.js";
 
-function registerRoutes() {
-    const app = express();
-
+function registerRoutes(app) {
     app.use("/api", productRoutes);
-    // app.use("/api", categoryRoutes);
+    app.use("/api", categoryRoutes);
     // app.use("/api", accountRoutes);
 }
 
