23980b02231c9a5b3f436476ff3cb64943b064b5
Minh <elevenine00@gmail.com>
2024-08-23 13:55:28 +0700

    add validation to input

--- app/auth/validator.js
+++ app/auth/validator.js
@@ -1,10 +1,13 @@
-import { object, string } from "joi";
+import joi from "joi";
 
-const userSchema = object({
-    username: string().alphanum().min(3).max(30).required(),
-    email: string().email().required(),
-    password: string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
-    role: string().valid("user", "admin"),
+const userSchema = joi.object({
+    username: joi.string().alphanum().min(3).max(30).required(),
+    email: joi.string().email().required(),
+    password: joi
+        .string()
+        .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+        .required(),
+    role: joi.string().valid("user", "admin"),
 });
 
 export function validateCreateUser(req, res, next) {
